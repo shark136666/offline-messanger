4fb50c010b6388acc602f5c33838eeb0e77b085c
Kuzma Molchanov <kuzma.molchanov@planarchel.ru>
2021-02-04 11:28:05 +0500

    update

--- sanic/init_database_table.sh
+++ sanic/init_database_table.sh
@@ -1,6 +1,1 @@
-echo "##### try create revision"
-alembic revision -m "init database_table" --autogenerate
-echo "##### try upgrade database"
 alembic upgrade head
-echo "Succes, but it is not exactly"
-
